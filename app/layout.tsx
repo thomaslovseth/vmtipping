d163afce09fa9302ae72b855d6a33f80656b6bdb
thomaslovseth <thomaslovseth@gmail.com>
2026-06-11 10:17:41 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'ØRST VM-TIPPING 2026',
-  description: 'FIFA World Cup 2026 tipping for Ørst-gjengen',
+  title: 'fotballtipping.no',
+  description: 'Nå: FIFA World Cup 2026',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
